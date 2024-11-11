57d0b716a5b62f988a85b7f8f622b74c698db8dc
arjunsdev17 <arjun.singh@razorpay.com>
2024-11-11 15:40:05 +0530

    implemented maps,enumeration and oneofs etc

--- go-protobuf/main.go
+++ go-protobuf/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 
 	pb "example.com/m/proto"
+	"google.golang.org/protobuf/proto"
 )
 
 func doSimple() *pb.Simple {
@@ -25,8 +27,70 @@ func doComplex() *pb.Complex {
 		},
 	}
 }
+func doEnum() *pb.Enumeration {
+	return &pb.Enumeration{
+		EyeColor: pb.EyeColor_EYE_COLOR_BLACK,
+	}
+}
+func doOneOf(messages interface{}) {
+	switch x := messages.(type) {
+	case *pb.Result_Id:
+		fmt.Println(messages.(*pb.Result_Id).Id)
+	case *pb.Result_Message:
+		fmt.Println(messages.(*pb.Result_Message).Message)
+	default:
+		fmt.Println("message has unexpected types :", x)
+	}
+}
+func doMap() *pb.MapsEx {
+	return &pb.MapsEx{
+		Ids: map[string]*pb.IdWrapper{
+			"mid1": {Id: 1},
+			"mid2": {Id: 2},
+			"mid3": {Id: 3},
+		},
+	}
+}
+func doFile(p proto.Message) {
+	path := "simple.bin"
+	writeToFile(path, p)
+	message := &pb.Simple{}
+	readFromFile(path, message)
+	fmt.Println(message)
+}
+func doToJSON(p proto.Message) string {
+	jsonString := toJSON(p)
+
+	return jsonString
+}
+func doFromJSON(jsonString string, t reflect.Type) proto.Message {
+	messaage := reflect.New(t).Interface().(proto.Message)
+	fromJSON(jsonString, messaage)
+	return messaage
+}
 func main() {
 	// fmt.Println("data :", doSimple())
+	// fmt.Println("data :", doComplex())
+	// fmt.Println("data :", doEnum())
 
-	fmt.Println("data :", doComplex())
+	// doOneOf(&pb.Result_Id{Id: 42})
+	// doOneOf(&pb.Result_Message{Message: "Jai shree Ram Bhiya "})
+
+	// // fmt.Println("do map :", doMap())
+	// fmt.Println("Data has been written :")
+	// doFile(doSimple())
+
+	// jsonString := doToJSON(doSimple())
+	// message := doFromJSON(jsonString, reflect.TypeOf(pb.Simple{}))
+	// fmt.Println(jsonString)
+	// fmt.Println("m :", message)
+
+	// jsonString = doToJSON(doComplex())
+	// message = doFromJSON(jsonString, reflect.TypeOf(pb.Complex{}))
+	// fmt.Println(jsonString)
+	// fmt.Println("m :", message)
+
+	// for checking DiscardOption//
+	fmt.Println("test :", doFromJSON(`{"id":42,"unknown":"test"}`, reflect.TypeOf(pb.Simple{})))
+
 }
